cc481aae80994d6f415c5d08b3ef09df487ba4c5
TreciaKS <tksekgalo@gmail.com>
2022-10-18 18:46:38 +0200

    created folder for navbar

--- frontend/components/Navbar/Navbar.js
+++ frontend/components/Navbar/Navbar.js
@@ -1,20 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className={styles.row}>
         <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
-            
-            {/* logo */}
-            <div>
-                <img
-                src='/logo1.png'
-                width='25px'
-                height='25px'
-                className="transition-colors duration-300 transform mr-10" />
-            </div>
-            
+          
             {/* menu items */}
             <Link href=''>
             <a className="border-b-2 border-transparent hover:text-blue-600 hover:border-red-800 transition-colors duration-300 transform dark:text-gray-200 mx-1.5 sm:mx-6 ">About</a>
