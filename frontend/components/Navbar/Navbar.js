866bd47b8fd67a481dc8a1c1c2a4f99a46094e32
TreciaKS <tksekgalo@gmail.com>
2022-10-19 10:52:18 +0200

    Navbar is okay with new font

--- frontend/components/Navbar/Navbar.js
+++ frontend/components/Navbar/Navbar.js
@@ -7,11 +7,11 @@ const Navbar = () => {
         <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
           
             {/* menu items */}
-            <a className="border-b-2 border-transparent hover:text-blue-600 hover:border-red-800 transition-colors duration-300 transform dark:text-gray-200 mx-1.5 sm:mx-6 ">About</a>
+            <a href='#' className="hover:border-blue-800 border-b-2 border-transparent hover:text-pink-600 transition-colors duration-300 transform mx-1.5 sm:mx-6">1UP <br /> 656 </a>
 
-            <a className="border-b-2 border-transparent hover:border-pink-800 hover:text-indigo-600 transition-colors duration-300 transform mx-1.5 sm:mx-6">Skills</a>
+            <a href='#' className="border-b-2 border-transparent hover:border-pink-800 hover:text-indigo-600 transition-colors duration-300 transform mx-1.5 sm:mx-6">HIGH SCORE <br /> 200113 </a>
 
-            <a className="hover:border-blue-800 border-b-2 border-transparent hover:text-pink-600 transition-colors duration-300 transform mx-1.5 sm:mx-6">Work</a>
+            <a href='#' className="hover:border-blue-800 border-b-2 border-transparent hover:text-pink-600 transition-colors duration-300 transform mx-1.5 sm:mx-6">2UP <br /> 1018</a>
         </div>
     </nav>
   )
