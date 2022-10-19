34b698f610ff66c1e750cdafc652e157a566ff72
TreciaKS <tksekgalo@gmail.com>
2022-10-19 10:51:42 +0200

    designed menu for navigation

---
+++ frontend/components/Menu/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Menu.module.css'
+
+const Menu = () => {
+  return (
+        <div class="wrap">
+            <div class="links">
+                <ul>
+                    <li><a href="#" class="link">About</a></li>
+                    <li><a href="#" class="link">Skills</a></li>
+                    <li><a href="#" class="link">Projects</a></li>
+                    <li><a href="#" class="link">Talks</a></li>
+                    <li><a href="#" class="link">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+  )
+}
+
+export default Menu
