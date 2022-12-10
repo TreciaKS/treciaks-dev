be9cb416803a442f6ecc6ebfeb681ffee633c564
TreciaKS <tksekgalo@gmail.com>
2022-12-10 11:04:40 +0200

    styled about me

--- frontend/pages/about/index.js
+++ frontend/pages/about/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const about = () => {
   return (
@@ -15,45 +17,44 @@ const about = () => {
         <link rel="icon" href="/logo1.png" />
       </Head>
       
-        <div className="relative flex">
-            <div className="min-h-screen lg:w-1/3"></div>
-            <div className="hidden w-3/4 min-h-screen lg:block"></div>
-            
-            <div
-                className="container flex flex-col justify-center w-full min-h-screen px-6 py-10 mx-auto lg:absolute lg:inset-x-0">
-                
-                <div className="mt-10 lg:mt-5 lg:flex lg:items-center">
-                    <img className="object-cover object-center rounded-full w-96 lg:w-96 h-96" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" />
+      <main className="flex items-center justify-center bg-black">
+          <div className="px-6 py-10 mx-auto lg:mt-32 md:mt-20">
+              <div className="lg:-mx-6 lg:flex lg:items-center">
+                  <Image
+                    src="https://images.unsplash.com/photo-1499470932971-a90681ce8530?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80"
+                    alt="Picture of the author"
+                    width={500}
+                    height={500}
+                    className='object-cover object-center lg:w-1/2 lg:mx-6 w-full h-96 rounded-full lg:h-[36rem] shadow shadow-amber-900'
+                  />
 
-                    <div className="mt-8 lg:px-10 lg:mt-0 lg:ml-8">
-                    <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl xl:text-5xl dark:text-white">
-                        What our <span class="text-blue-500">customers</span> <br /> are saying
-                    </h1>
-                    <p className='mt-10'></p>
-                        <h1 className="text-2xl font-semibold text-gray-800 dark:text-white lg:w-72">
-                            Help us improve our productivity
-                        </h1>
+                  <div className="mt-8 lg:w-1/2 lg:px-6 lg:mt-0">
+                      <h1 className="text-3xl font-semibold text-violet-600 lg:text-4xl lg:w-96">
+                          Help us improve our productivity 🐜
+                      </h1>
 
-                        <p className="max-w-lg mt-6 text-gray-500 dark:text-gray-400">
-                            “ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero ad
-                            tempora doloribus expedita laborum saepe voluptas perferendis delectus assumenda rerum, culpa
-                            aperiam dolorum, obcaecati corrupti aspernatur a. ”
+                      <p className="max-w-lg mt-6 text-gray-500 dark:text-gray-400 ">
+                          “ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero ad
+                          tempora doloribus expedita laborum saepe voluptas perferendis delectus assumenda rerum, culpa
+                          aperiam dolorum, obcaecati corrupti aspernatur a. ”
+                      </p>
 
-                            “ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero ad
-                            tempora doloribus expedita laborum saepe voluptas perferendis delectus assumenda rerum, culpa
-                            aperiam dolorum, obcaecati corrupti aspernatur a. ”
+                      <h3 className="mt-6 text-lg font-medium text-yellow-500">Mia Brown</h3>
+                      <p className="text-gray-600 dark:text-gray-300">Marketing Manager at Stech</p>
 
-                            “ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero ad
-                            tempora doloribus expedita laborum saepe voluptas perferendis delectus assumenda rerum, culpa
-                            aperiam dolorum, obcaecati corrupti aspernatur a. ”
-                        </p>
+                      <div className="flex items-center justify-between mt-12 lg:justify-start">
+                          <button className="p-2 px-4 py-2 text-gray-200 transition-colors duration-300 border border-teal-700 rounded-full rtl:-scale-x-100 lg:mx-6 hover:text-teal-700">
+                             <Link href={'/'}>Home</Link>
+                          </button>
 
-                        <h3 className="mt-6 text-lg font-medium text-blue-500">Ronik Ederson</h3>
-                        <p className="text-gray-600 dark:text-gray-300">Marketing Manager at Stech</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+                          <button className="p-2 px-4 py-2 text-gray-200 transition-colors duration-300 border border-indigo-700 rounded-full rtl:-scale-x-100 lg:mx-6 hover:text-indigo-700">
+                             <Link href={'/skills'}> Skills</Link>
+                          </button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </main>
     </section>
   )
 }
